946ca79891cc08bdc86ddfaeed294f6e232b8c09
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-02-27 19:22:48 -0600

    instalacion de vite  y configuracion.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], // trabaje con todos los archivos html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
